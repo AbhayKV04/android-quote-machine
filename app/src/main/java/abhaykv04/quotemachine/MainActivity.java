6c9ffb9733443a620933f3423a0e4177d4ca14e2
AbhayKV04 <AbhayKV04@gmail.com>
2017-07-08 22:15:04 +0530

    Add menu in action bar

--- app/src/main/java/abhaykv04/quotemachine/MainActivity.java
+++ app/src/main/java/abhaykv04/quotemachine/MainActivity.java
@@ -3,6 +3,8 @@ package abhaykv04.quotemachine;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -78,5 +80,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+
+            case R.id.exit:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 }
